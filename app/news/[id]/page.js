4df236e68b4baba0bf5231579694f9261b658474
cam lowndes <cameronlowndes@gmail.com>
2025-06-12 11:38:02 +0100

    my second commit

---
+++ app/news/[id]/page.js
@@ -0,0 +1,3 @@
+export default function NewsDetailPage(){
+    return <h1>This the NewsDetailPage.</h1>
+}
---
+++ app/news/page.js
@@ -0,0 +1,3 @@
+export default function NewsPage() {
+    return <h1>This is the NewsPage.</h1>
+}
